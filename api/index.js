b76c6357bee836b8e8db00e57a44737956b7f1f3
armandogman <armandogman880@gmail.com>
2023-09-08 17:21:35 -0500

    First commit

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const Transaction = require('./models/Transaction.js');
+const { default: mongoose } = require("mongoose");
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Parse JSON request bodies
+app.use(express.json());
+
+const port = 4000; // Define the port number
+
+app.get('/api/test', (req, res) => {
+  res.json({ body: 'test ok3' });
+});
+
+app.post('/api/transaction', async (req, res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const {name,description,datetime,price} = req.body;
+  const transaction = await Transaction.create({name, description,datetime,price});
+  res.json(transaction);
+});
+
+app.get('/api/transactions', async(req,res) => {
+  await mongoose.connect(process.env.MONGO_URL);
+  const transactions = await Transaction.find();
+  res.json(transactions);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
